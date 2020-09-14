29d52fc90675350af78589512271f09e03040a54
Dylan Turnbull <dylanturn@gmail.com>
2020-09-14 18:03:27 -0500

    using requirementslib

--- setup.py
+++ setup.py
@@ -1,30 +1,9 @@
 import setuptools
-import toml
 import os
+from requirementslib import Lockfile
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-
-
-def get_install_requirements():
-    try:
-        # read my pipfile
-        with open ('Pipfile', 'r') as fh:
-            pipfile = fh.read()
-        # parse the toml
-        pipfile_toml = toml.loads(pipfile)
-    except FileNotFoundError:
-        return []    # if the package's key isn't there then just return an empty
-    # list
-    try:
-        required_packages = pipfile_toml['packages'].items()
-    except KeyError:
-        return []
-
-    # If a version/range is specified in the Pipfile honor it
-    # otherwise just list the package
-    return ["{0}{1}".format(pkg,ver) if ver != "*"
-        else pkg for pkg,ver in required_packages]
 
 setuptools.setup(
     name="kubeasy-py",
@@ -42,5 +21,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.8',
-    install_requires = get_install_requirements()
+    install_requires = Lockfile.load("Pipfile.lock").as_requirements()
 )
